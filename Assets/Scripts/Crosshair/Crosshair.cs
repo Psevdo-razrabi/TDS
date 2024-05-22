e398b822990f4ba9a9b07a4b4c815730e4777bd2
unBeam <beamdev@yandex.ru>
2024-05-23 02:57:45 +0500

    настроенная сцена

--- Assets/Scripts/Crosshair/Crosshair.cs
+++ Assets/Scripts/Crosshair/Crosshair.cs
@@ -13,7 +13,6 @@ public class Crosshair : MonoBehaviour
     private void Awake()
     {
         Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
